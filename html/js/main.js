bcde9d64e60a230de290bcc88393cfbff802dc34
Pajaani Ajaghen Soobrayen <pajaani@bitvillegaming.com>
2022-01-24 09:47:30 +0400

    init

---
+++ html/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('.text-trunc').Cuttr({
+        //options here
+        truncate: 'words',
+        length: 15
+    });
+
+
+
+    var menu = $('.menu');
+    var origOffsetY = menu.offset().top;
+
+    document.onscroll = function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('.menu').addClass('fixed-top');
+        } else {
+            $('.menu').removeClass('fixed-top');
+        }
+    };
+
+
+    $('.carousel-home').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+      });
+
+});
